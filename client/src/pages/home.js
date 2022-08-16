e4248c2f67dacbfa6c9a9345314329b0b3cbd57e
divya g <aiswarya2002aishu@gmail.com>
2022-08-16 14:45:01 +0530

    jst

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -10,17 +10,7 @@ const Home = () =>{
   return(
       <div className="home">
         <div className="slideShowHome">
-          <Carousel>
-            <Carousel.Item interval={1000}>
-              <img className="d-block w-100" src="https://www.creditboss.in/wp-content/uploads/2020/10/Process-of-funding.jpg" height="500"  alt="First slide"/>
-            </Carousel.Item>
-            <Carousel.Item interval={500}>
-              <img className="d-block w-100" src="https://d1ngglk168riv7.cloudfront.net/n/wp-content/uploads/2020/04/30092032/qatarfoundationcover.jpg" height="500" alt="Second slide"/>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img className="d-block w-100" src="https://i2.cdn.turner.com/cnn/dam/assets/120416063143-education-funding-story-top.jpg" height="500"  alt="Third slide"/>
-            </Carousel.Item>
-          </Carousel>
+        <img className="d-block w-100" src="https://www.creditboss.in/wp-content/uploads/2020/10/Process-of-funding.jpg" height="500"  alt="First slide"/>
         </div>
         
         <div className="marqueeScrollHome">
@@ -84,3 +74,14 @@ const Home = () =>{
 }
 
 export default Home
+/* <Carousel>
+            <Carousel.Item interval={1000}>
+              <img className="d-block w-100" src="https://www.creditboss.in/wp-content/uploads/2020/10/Process-of-funding.jpg" height="500"  alt="First slide"/>
+            </Carousel.Item>
+            <Carousel.Item interval={500}>
+              <img className="d-block w-100" src="https://d1ngglk168riv7.cloudfront.net/n/wp-content/uploads/2020/04/30092032/qatarfoundationcover.jpg" height="500" alt="Second slide"/>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="d-block w-100" src="https://i2.cdn.turner.com/cnn/dam/assets/120416063143-education-funding-story-top.jpg" height="500"  alt="Third slide"/>
+            </Carousel.Item>
+          </Carousel>*/
