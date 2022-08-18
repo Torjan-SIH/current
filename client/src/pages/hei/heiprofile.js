97bbdfa3be0f8b70a6700fd778d51e0d88a6e51d
unknown <sirishakamunuri@gmail.com>
2022-08-18 22:21:10 +0530

    alignment

--- client/src/pages/hei/heiprofile.js
+++ client/src/pages/hei/heiprofile.js
@@ -11,7 +11,7 @@ const HeiProfile = () => {
                 <HeiSideBar/>
                 <form className="heiProfileForm">
                     <h1 className="heiProfileHeading">HEI Profile</h1>
-                    <table className="heiProfileTable" cellPadding={10}>
+                    <table className="heiProfileTable" cellPadding={10} >
                     <tr>
                             <th>ID: </th>
                             <td><input type="text" name="id"  disabled value="IND001" className="inputBox"></input></td>
